be23f5a7bf6ac38b4f52cdc2a44dd896becc7e7b
Philip Bendiksen <philipbendiksen@gmail.com>
2024-02-22 14:36:33 +0100

    Setup

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App";
+import Explorer from "./Explorer";
+import Search from "./Search";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" Component={App}>
+      <Route index element={<Navigate to="explorer" />} />
+      <Route path="explorer" Component={Explorer} />
+      <Route path="search" Component={Search} />
+      <Route path="*" element={<span>404</span>} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
